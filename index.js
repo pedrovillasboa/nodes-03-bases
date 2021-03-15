7da6b5677bb9e52cc9d3a96a774fee15bcfef950
pedro.villasboa <pedro.villasboa@gmail.com>
2021-03-15 18:20:25 -0300

    se agreagron dependencias yargs y colors

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 
+const argv = require('yargs')
+       
+            .option('b', {
+                alias:'base',
+                type:'number',
+                demandOption: true,
+                describe: ' es la base de la tabla de multiplicar'
+            })
 
-const   base= 6;
+
+            .option('l', {
+                alias:'limite',
+                type:'number',
+                demandOption: false,
+                default:10,
+                describe: ' es el  limite de multiplicador'
+            })
+
+            .option('v', {
+                alias:'visualizar',
+                type:'booleano',
+                demandOption: false,
+                default:false,
+                describe: ' muestra la tabla en pantalla'
+            })
+
+             .check((argv , option) =>{
+                 if(isNaN(argv.b)){
+                     throw new Error('La base tiene que ser un numero')
+                 }
+                 return true
+             })
+             
+
+            .help() 
+            .version() 
+
+        .argv;
+
+
 const  {multiplicar} = require("./multiplicador");
 
-multiplicar(base);
+
+multiplicar(argv.b,argv.l, argv.v);
 
--- multiplicador/index.js
+++ multiplicador/index.js
@@ -1,14 +1,24 @@
+const colors = require('colors');
 const fs = require('fs');
 const {logica} = require('./logica');
 
-const multiplicar = (base) => {
-    console.log(`*********tabla del ${base}
+const multiplicar = (base, limite,visualizar) => {
+   
+      const  resultado = logica(base , limite);
+    if(visualizar){
+        console.log(`*********tabla del ${base}
     ******* `);
+ console.log(resultado);
 
-      const  resultado = logica(base);
+    }
+ 
     
-     console.log(resultado);
-    fs.writeFileSync(`tablas/tabla del ${base}`,resultado);
+     const  nombreArchivo = ` tabla-del- ${base} .txt`
+
+    fs.writeFile(`tablas/ ${nombreArchivo}`,resultado, (error) =>{
+        if(error) throw error;
+        console.log(`el archivo ${nombreArchivo} fue creado!`.rainbow);
+    });
     
     
 
